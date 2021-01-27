beb3e9a9df6e5e795ec52fdd2b12870770a3aa47
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-01-28 00:19:23 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,25 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
+    public static Stats getStatistics(final List<Float> numbers) {
+    //implement the computation of statistics here
+    if (numbers.isEmpty())
+    {
+      stats.setAverage(Float.NaN);
+      stats.setMin(Float.NaN);
+      stats.setMax(Float.NaN);
+    }
+		else
+        {
+			stats.setMin(Collections.min(numbers));
+			stats.setMax(Collections.max(numbers));
+			int listLength=numbers.size();
+            Float sumofnums=0.0f;
+            for(Float i:numbers){
+                sumofnums+=i;
+            }
+            s.setAverage((Float)sumofnums/listLength);
+        }
+        return s;
     }
 }
