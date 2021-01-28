47498ca2dc344e747d3e76718c96ee114a9c16c4
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-01-29 00:06:22 +0530

    Update EmailAlert.java

--- src/main/java/statisticker/EmailAlert.java
+++ src/main/java/statisticker/EmailAlert.java
@@ -3,6 +3,11 @@ package statisticker;
 public class EmailAlert implements IAlerter {
 
 	public boolean emailSent;
+	
+	public void alert(boolean emailSent)
+	{
+		this.emailSent = emailSent;
+	}
 
 }
 
